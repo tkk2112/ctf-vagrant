152049eb7afc6881da934c568c86e5e0a7b53a2b
Thomas Kristensen <thkriste@cisco.com>
2016-11-11 17:10:15 +0100

    updated scripts

--- run.sh
+++ run.sh
@@ -1,1 +1,8 @@
-docker run --rm -t -i garble/ctf /sbin/my_init -- bash -l
+#!/bin/bash
+
+if [ -z "$1" ]; then
+    echo "$ run.sh /path/to/shared/folder"
+    exit 1
+fi
+
+docker run -v $1:/root/host-share --rm -t -i garble/ctf /sbin/my_init -- bash -l
