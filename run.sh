b3ef57d59032a0b9f12a56b31a310382de2dfb06
Thomas Kristensen <tkristensen@gmail.com>
2016-11-12 23:09:16 +0100

    Fixes ssh and startup script.

--- run.sh
+++ run.sh
@@ -5,4 +5,8 @@ if [ -z "$1" ]; then
     exit 1
 fi
 
-docker run -v $1:/root/host-share --rm -it --privileged --workdir=/root garble/ctf
+docker run -v $1:/root/host-share -p 8022:22 -d --privileged --workdir=/root --name=ctf --rm garble/ctf
+
+script=$(pwd)
+echo ""
+$script/docker-ssh
